d38b72c1005b50379f6f87a85177ebb1c02aae79
jack <jackmanzer88@gmail.com>
2021-10-05 14:16:57 -0500

    cleaned up javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,29 +10,31 @@ const weatherEl = document.getElementById('weather');
 const flagEl = document.getElementById('flag');
 
 //Global Variables 
-let placeName = 'austin';
-const geoNamesUsername = 'matmll12';
+let placeName; 
 let countryCode;
-let flagUrl = `https://www.countryflags.io/${countryCode}/shiny/64.png`;
+let flagUrl;
 let start;
 let end;
 let currentWeatherData;
 let pixabayImg;
+let temp; 
 
-//callGeoNamesAPI
+//declare GeoNamesAPI API function
 
-const callGeoNamesAPI = function () {
+function callGeoNamesAPI() {
 
+    const geoNamesUsername = 'matmll12';
     const apiUrl = `http://api.geonames.org/searchJSON?q=&name_equals=${placeName}&maxRows=10&username=${geoNamesUsername}`;
 
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                // console.log('data = ' + data);
+                //retrieves country code 
                 countryCode = data.geonames[0].countryCode;
+                //generates flag icon url 
                 flagUrl = `https://www.countryflags.io/${countryCode}/shiny/64.png`;
+                //calls pixabay api 
                 callPixabayAPI();
-                console.log(flagUrl);
             });
         } else {
             alert(`Error: ${response.statusText}`);
@@ -40,7 +42,7 @@ const callGeoNamesAPI = function () {
     })
 }
 
-//callPixabay
+//declare Pixbay API function
 
 function callPixabayAPI() {
 
@@ -49,11 +51,10 @@ function callPixabayAPI() {
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                // console.log(data.hits[0].largeImageURL);
+                //retrieve first image from pixabay array
                 pixabayImg = data.hits[0].largeImageURL;
-                // callCurrentWeatherDataAPI();
-                renderDatabox();
-
+                //renders data inside databox
+                callCurrentWeatherDataAPI(placeName)
             });
         } else {
             alert(`Error: ${response.statusText}`);
@@ -61,9 +62,9 @@ function callPixabayAPI() {
     })
 };
 
-//make Weather API call
+//declare Weather API function
 
-const callCurrentWeatherDataAPI = function(cityName){
+function callCurrentWeatherDataAPI(cityName){
   
     const apiKey = 'f2d872dec206d66d9deec95927164a7b';
     const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
@@ -72,7 +73,7 @@ const callCurrentWeatherDataAPI = function(cityName){
         if(response.ok){
             response.json().then(function(data){
                 currentWeatherData = data;
-                console.log(data);
+                temp = Math.round(currentWeatherData.main.temp);
                 renderDatabox();
             });
         } else {
@@ -80,6 +81,8 @@ const callCurrentWeatherDataAPI = function(cityName){
         };
     })
 };
+
+//declare renderDatabox function
 
 function renderDatabox(){
     cityEl.textContent = placeName;
@@ -89,30 +92,30 @@ function renderDatabox(){
     <p>${start} - ${end}</p>
     `;
     // weatherEl.
-    databoxEl.classList.remove('hide');
+    weatherEl.innerHTML =  `<p class="temp">Temp: ${temp} &#8457;</p>`
 
+    databoxEl.classList.remove('hide');
 }
 
+//add event listeners 
 
+//for modal 
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems, "");
 });
 
+//form submit listener 
 formEl.addEventListener('submit', function (event) {
     event.preventDefault();
     placeName = formEl.placename.value.trim();
-    
-    console.log(formEl.placename.value);
-    console.log(placeName);
     start = formEl.startDate.value;
-    console.log(start)
     end = formEl.endDate.value;
-    console.log(end)
     callGeoNamesAPI();
-    callCurrentWeatherDataAPI(placeName);
-    // renderDatabox();
-    // databoxEl.classList.remove('hide');
-
 });
 
+//API Call Sequence 
+
+//call geonamesAPI which set the flag url calls PixaBay api which calls current weather API which calls render function. 
+
+//watch out for spaces 
